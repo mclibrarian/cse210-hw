60dd0e0b0ec6eae6ab55266f2ec25a98eab7de96
Brita McNabb <brita@mcnabbs.org>
2025-01-16 22:30:11 -0500

    added preparation assignments 3-5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int response;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            string userGuess = Console.ReadLine();
+            response = int.Parse(userGuess);
+            if (response > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (response < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (response == number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (response != number);
     }
 }
